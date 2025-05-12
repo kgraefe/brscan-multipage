a367405fcde4d074dd837ff49155ae222a9a9d35
Konrad Vité <kvite@paktolos.net>
2025-05-12 12:16:24 +0200

    Add colored logging
    Signed-off-by: Konrad Vité <kvite@paktolos.net>

--- brscan_multipage.py
+++ brscan_multipage.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import logging
 import asyncio
 import argparse
+import colorlog
 
 SNMP_UDP_PORT = 161
 COMMAND_UDP_PORT = 54925  # Brother expects this and nothing else
@@ -10,11 +10,11 @@ ADVERTISE_INTERVAL = 30
 RESOLUTION = 200
 RETRIES = 2
 
-logger = logging.getLogger()
+log = colorlog.getLogger("brscan_multipage")
 
 
 async def main(args):
-    logger.info("Hello world")
+    log.info("Hello world")
 
 
 if __name__ == "__main__":
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         help="""
             Host IP address that should be advertised to the scanner. This defaults to
             listen_addr but might be different in NATed or containered environments.
-            """,
+    """,
     )
     parser.add_argument(
         "scanner_addr", type=str, help="IP address or DNS entry of printer"
@@ -43,9 +43,17 @@ if __name__ == "__main__":
     parser.add_argument("output_dir", type=str, help="output directory")
     args = parser.parse_args()
 
-    logging.basicConfig(
-        level=args.loglevel.upper(),
-        format="%(asctime)s - %(levelname)s - %(message)s",
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(
+        colorlog.ColoredFormatter(
+            "%(light_black)s%(asctime)s "
+            "%(blue)s%(name)s[%(process)d] "
+            "%(log_color)s%(levelname)-8s "
+            "%(message)s%(reset)s"
+        )
     )
+
+    log.setLevel(args.loglevel.upper())
+    log.addHandler(handler)
 
     asyncio.run(main(args))
